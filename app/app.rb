f2e55f4ba7124b22cfdfe706084af6859c2d540f
benjamin-white <benjaminwhite@live.co.uk>
2016-07-28 16:53:28 +0100

    step 20, adds password fail flash

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'sinatra/flash'
 require_relative 'data_mapper_setup'
 
 
@@ -7,6 +8,7 @@ ENV["RACK_ENV"] ||= "development"
 class Bookmark_manager < Sinatra::Base
   enable :sessions
   set :session_secret, 'super secret'
+  register Sinatra::Flash
 
   helpers do
     def current_user
@@ -41,15 +43,21 @@ class Bookmark_manager < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(email: params[:email],
+    @user = User.create(email: params[:email],
                 password: params[:password],
                 password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
+    if @user.save
+      session[:user_id] = @user.id
       redirect to('/links')
+    else
+      flash.now[:notice] = 'Password and confirmation password do not match'
+      erb :'/users/new'
+    end
   end
 
   # start the server if ruby file executed directly
--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -9,6 +9,8 @@ feature 'user registration' do
 
   scenario 'incorrect password denies user registration' do
     expect{ sign_up(password_confirmation: 'cats') }.not_to change(User, :count)
+    expect(current_path).to eq '/users'
+    expect(page).to have_content 'Password and confirmation password do not match'
   end
 
 end
